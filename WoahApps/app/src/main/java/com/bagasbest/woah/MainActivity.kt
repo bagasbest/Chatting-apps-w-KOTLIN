6d21772fb3eaf299ed59248077b98f611acb8bd3
bagasbest <bagasbest80@gmail.com>
2020-10-27 16:58:24 +0700

    form validation

--- WoahApps/app/src/main/java/com/bagasbest/woah/MainActivity.kt
+++ WoahApps/app/src/main/java/com/bagasbest/woah/MainActivity.kt
@@ -4,20 +4,55 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.util.Patterns
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.etEmail
+import kotlinx.android.synthetic.main.activity_main.etPassword
+import kotlinx.android.synthetic.main.activity_register.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val email = etEmail.text.toString()
-        val password = etPassword.text.toString()
+        btnLogin.setOnClickListener {
+            validateEmail()
+            validatePassword()
 
-        Log.d("MainActivity", "Email is : $email")
-        Log.d("MainActivity", "Password is : $password")
+            Log.d("MainActivity", "Email is : ${etEmail.text.toString()}")
+            Log.d("MainActivity", "Password is : ${etPassword.text.toString()}")
+        }
+    }
 
+    private fun validateEmail(): Boolean {
+        val email = etEmail.text.toString().trim()
+
+        return if(email.isEmpty()) {
+            etEmail.error = "Field can't be empty"
+            false;
+        }else if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            etEmail.error = "Please enter a valid email address"
+            false
+        }else {
+            etEmail.error = null
+            true
+        }
+    }
+
+    private fun validatePassword (): Boolean {
+        val password = etPassword.text.toString().trim()
+
+        return if(password.isEmpty()){
+            etPassword.error = "Field can't be empty"
+            false
+        } else if (!RegisterActivity.PASSWORD_PATTERN?.matcher(password)?.matches()!!) {
+            etPassword.error = "Password too weak"
+            false
+        } else {
+            etPassword.error = null
+            true
+        }
     }
 
     fun forgotPassword(view: View) {
--- WoahApps/app/src/main/java/com/bagasbest/woah/RegisterActivity.kt
+++ WoahApps/app/src/main/java/com/bagasbest/woah/RegisterActivity.kt
@@ -4,29 +4,99 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.util.Patterns
+import android.util.Patterns.EMAIL_ADDRESS
 import android.view.View
 import kotlinx.android.synthetic.main.activity_register.*
+import java.util.regex.Pattern
 
 
 class RegisterActivity : AppCompatActivity() {
+
+
+    companion object {
+        val PASSWORD_PATTERN: Pattern? =
+            Pattern.compile("^" +
+                    "(?=. *[0-9])" +
+                    "(?=. *[a-z])" +
+                    "(?=. *[A-Z])" +
+                    "(?=. *[@#%^&+=])" +
+                    "(?=\\S+$)" +
+                    ".{6,}" +
+                    "$")
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
         btnRegister.setOnClickListener {
-            val email = etEmail.text.toString()
-            val name = etName.text.toString()
-            val password = etPassword.text.toString()
 
-            Log.d("RegisterActivity", "Email is : $email\nNama Lengkap is : $name\nPassword is : $password\n")
+            validateEmail()
+            validateName()
+            validatePassword()
+
+           Log.d("RegisterActivity", "Email is : ${etEmail.text.toString()}\nNama Lengkap is : ${etName.text.toString()}\nPassword is : ${etPassword.text.toString()}\n")
 
         }
 
 
     }
 
+     private fun validateEmail(): Boolean {
+        val email = etEmail.text.toString().trim()
+
+         return if(email.isEmpty()) {
+             etEmail.error = "Field can't be empty"
+             false;
+         }else if(!EMAIL_ADDRESS.matcher(email).matches()){
+             etEmail.error = "Please enter a valid email address"
+             false
+         }else {
+             etEmail.error = null
+             true
+         }
+
+    }
+
+    private fun validateName (): Boolean {
+        val name = etName.text.toString().trim()
+
+        return if(name.isEmpty()){
+            etName.error = "Field can't be empty"
+            false
+        }else if (name.length > 15) {
+            etName.error = "Username too long"
+            false
+        } else {
+            etName.error = null
+            true
+        }
+    }
+
+    private fun validatePassword (): Boolean {
+        val password = etPassword.text.toString().trim()
+
+        return if(password.isEmpty()){
+            etPassword.error = "Field can't be empty"
+            false
+        } else if (!PASSWORD_PATTERN?.matcher(password)?.matches()!!) {
+            etPassword.error = "Password too weak"
+            false
+        } else {
+            etPassword.error = null
+            true
+        }
+
+    }
+
+
     fun backLogin(view: View) {
-        startActivity(Intent(this, MainActivity::class.java))
+        val intent = Intent(this, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        startActivity(intent)
+        finish();
     }
 
 }
